4c93625d88b1ab0ebaae93c6d172baea8f414e47
romeroivar03 <ivar_romero@hotmail.com>
2022-09-01 19:42:46 -0500

    Consola Add Municipio-DT

--- Torneo.App/Torneo.App.Consola/Program.cs
+++ Torneo.App/Torneo.App.Consola/Program.cs
@@ -4,10 +4,56 @@ namespace Torneo.App.Consola
 {
     class Program
     {
-        
+        private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
+        private static IRepositorioDT _repoDT = new RepositorioDT();
+
         static void Main(string[] args)
         {
-          
+            int opcion = 0;
+            do
+            {
+                Console.WriteLine("1. Insertar Municipio");
+                Console.WriteLine("2. Insertar DT");
+                Console.WriteLine("0. Salir");
+                opcion = Int32.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        AddMunicipio();
+                        break;
+                    case 2:
+                        AddDT();
+                        break;
+                }
+            } while (opcion != 0);
+        }
+
+        private static void AddMunicipio()
+        {
+            Console.WriteLine("Ingrese el nombre del Municipio");
+            string nombre = Console.ReadLine();
+            var municipio = new Municipio
+            {
+                Nombre = nombre,
+            };
+            _repoMunicipio.AddMunicipio(municipio);
+        }
+
+        private static void AddDT()
+        {
+            Console.WriteLine("Ingrese el nombre del DT");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el documento del DT");
+            string documento = Console.ReadLine();
+            Console.WriteLine("Ingrese el telefono del DT");
+            string telefono = Console.ReadLine();
+            var directorTecnico = new DirectorTecnico
+            {
+                Nombre = nombre,
+                Documento = documento,
+                Telefono = telefono,
+            };
+            _repoDT.AddDT(directorTecnico);
         }
     }
 }

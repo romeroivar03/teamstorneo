0296b45eda4d7a5568c7af952c7d7ebaff6fde4a
romeroivar03 <ivar_romero@hotmail.com>
2022-09-17 23:16:58 -0500

    Editar Municipio/Equipo

--- Torneo.App/Torneo.App.Dominio/Entidades/Equipo.cs
+++ Torneo.App/Torneo.App.Dominio/Entidades/Equipo.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 namespace Torneo.App.Dominio
 {
     public class Equipo
     {
         public int Id { get; set; }
+        [Display(Name = "Nombre del Equipo")]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         public string Nombre { get; set; }
         public Municipio Municipio { get; set; }
         public DirectorTecnico DirectorTecnico { get; set; }

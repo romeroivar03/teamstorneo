1edb2f34a14ba2d68753c653aa94865d281c1244
YENNYFER <yrendon9@misena.edu.co>
2022-09-22 19:34:37 -0500

    Modificación Partidos

--- Torneo.App/Torneo.App.Persistencia/AppRepositorios/IRepositorioMunicipio.cs
+++ Torneo.App/Torneo.App.Persistencia/AppRepositorios/IRepositorioMunicipio.cs
@@ -6,6 +6,6 @@ namespace Torneo.App.Persistencia
         public Municipio AddMunicipio(Municipio municipio);
         public IEnumerable<Municipio> GetAllMunicipios();
         public Municipio GetMunicipio(int idMunicipio);
-         public Municipio UpdateMunicipio(Municipio municipio);
+        public Municipio UpdateMunicipio(Municipio municipio);
     }
 }
